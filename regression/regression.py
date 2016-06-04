985ca4459e1117c42ebc4edffc398267d315fb41
Jacob Mastel <jacob@mastel.org>
2016-06-04 16:57:21 -0700

    first attempt at regression

---
+++ regression/regression.py
@@ -0,0 +1,14 @@
+import numpy
+import statsmodels.api as sm
+# from numpy import matrix, polyfit
+
+# build the matricies, adding a ones column
+# to x for higher precision
+x = numpy.loadtxt('../sample_features.csv', delimiter="," , ndmin=2)
+o = numpy.ones((x.shape[0],1))
+x = numpy.concatenate((x, o), axis=1)
+y = numpy.loadtxt('../sample_target.csv', delimiter=",", ndmin=2)
+
+with open('out.txt', 'w') as f:
+	results = sm.OLS(y, x).fit().summary()
+	print results
