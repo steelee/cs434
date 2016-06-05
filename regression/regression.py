8c8b798ab31e90a3daee7f3c21c40176e750cfbd
Jacob Mastel <jacob@mastel.org>
2016-06-04 19:12:19 -0700

    created a % correct check

--- regression/regression.py
+++ regression/regression.py
@@ -10,7 +10,24 @@ x = numpy.concatenate((x, o), axis=1)
 y = numpy.loadtxt('../sample_target.csv', delimiter=",", ndmin=2)
 
 # perform a linear regresion on the data
+results = sm.OLS(y, x).fit()
+weights = results.params
+summary = results.summary().as_text()
+
 with open('linear_out.txt', 'w') as f:
-	results = sm.OLS(y, x).fit().summary().as_text()
-	f.write(results)
+	f.write(summary)
 	f.write("\n")
+
+success = 0
+failure = 0
+for i in range(x.shape[0]):
+	val = int(numpy.round(numpy.dot(weights, x[i])))
+	if val == y[i]:
+		success += 1
+	else:
+		failure += 1
+
+total = success + failure
+percent_correct = int((float(success) / float(failure)) * 100)
+print "success\tfailure\ttotal\tpercent correct"
+print "{0}\t{1}\t{2}\t{3}%".format(success, failure, total, percent_correct)
